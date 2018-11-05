fd880d14dfc48aaee018334d05e98b1d851e85dd
Roberto Honda <roberto_junior_honda@hotmail.com>
2018-11-05 09:18:39 -0300

    reafactor(index): remove unused link import

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const Test = () => <Router>
   <Switch>
