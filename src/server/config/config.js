4f6de9c709f91c93daa7d39a2b0750d1f7f4619c
Roberto Honda <roberto_junior_honda@hotmail.com>
2018-11-09 15:43:57 -0300

    just add NODE_ENV variable

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+export const NODE_ENV = process.env.NODE_ENV;
 export const PORT = process.env.SERVER_PORT;
 export const DB_URI = process.env.DB_URI;
 
